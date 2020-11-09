ac1f74825e28ead3fa12ae42d56258037f6b4211
Sanjeev M <sanjeev.malagi@scripbox.com>
2020-11-10 00:30:15 +0530

    Add module declaration

---
+++ types/index.d.ts
@@ -0,0 +1,25 @@
+import React from 'react';
+
+declare module 'react-native-permanent-storage' {
+  export interface PermanentStorageStatic {
+    getItem(key: string, callback?: (error?: Error, result?: string) => void): string | null;
+
+    setItem(key: string, value: string, callback?: (error?: Error) => void): void;
+
+    removeItem(key: string, callback?: (error?: Error) => void): void;
+
+    clear(callback?: (error?: Error) => void): void;
+  }
+
+  export function usePermanentStorage(key: string): {
+    getItem(callback?: (result: string | null) => void): string | null;
+    setItem(value: string, callback?: (error?: Error) => void): void;
+    removeItem(back?: (error?: Error) => void): void;
+  }
+
+  export const PermanentStorageWrapper: (props: { children: any }) => React.FC<{ children: any }>
+
+  const PermanentStorage: PermanentStorageStatic;
+
+  export default PermanentStorage;
+}
